c87e528e9433efcf5e7af71753e92e86a2444282
https://github.com/ShindePratikshaP <shindepratikshapratap9@gmail.com>
2026-03-30 19:10:52 +0530

    fetching data from backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import './App.css'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  
+  const loadTodos = async () => {
+    console.log('Loading todos...');
+
+    const response = await axios.get('http://localhost:8020/todos');
+    setTodos(response.data);
+  };
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
   return (
-    <div>App</div>
+    <div>
+      <h1>To-Do List</h1>
+      {todos.map((todo, index) => (
+        <div key={index} className="todo-card" >
+          <h3>{todo}</h3>
+        </div>
+      ))}
+    </div>
   )
 }
 
