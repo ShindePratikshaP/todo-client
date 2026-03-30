d25187ae5bcc7535752d08eeea197c1f8390514a
https://github.com/ShindePratikshaP <shindepratikshapratap9@gmail.com>
2026-03-30 20:04:02 +0530

    delete and edit API callig

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
 import React from 'react'
 import './App.css'
+import deleteIcon from './assets/delete.png';
+import editIcon from './assets/edit.png';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+  const [oldtodo,setOldTodos] =useState("");
+  const[editmode,setEditmode]= useState(false);
   
+  const BASE_URL = 'http://localhost:8020';
+
   const loadTodos = async () => {
     console.log('Loading todos...');
 
-    const response = await axios.get('http://localhost:8020/todos');
+    const response = await axios.get(`${BASE_URL}/todos`);
     setTodos(response.data);
   };
 
   const addTodo = async () => {
-    const response = await axios.post('http://localhost:8020/todos', { todo: newTodo });
+    if(!newTodo.trim()) return;
+    const response = await axios.post(`${BASE_URL}/todos`, { todoitem: newTodo });
     setNewTodo("");
+    loadTodos();
+  };
+
+  const editTodo = async () => {
+    const response = await axios.put(`${BASE_URL}/todos`, { oldtodo: oldtodo, newtodo: newTodo });
+    setNewTodo("");
+    setEditmode(false);
+    loadTodos();
+    setOldTodos("");
+  };
+
+  const deleteTodo = async (todo) => {
+    const response = await axios.delete(`${BASE_URL}/todos`, { data: { todoitem: todo } });
     loadTodos();
   };
 
@@ -27,10 +47,22 @@ function App() {
   return (
     <div>
       <h1>To-Do List</h1>
+      <p>{editmode ? 'Editing Todo' : 'Add todo'}</p>
       <div className='todo-items-container'>
       {todos.map((todo, index) => (
         <div key={index} className="todo-card" >
           <h3>{todo}</h3>
+          <div>
+            <img src={editIcon} alt="Edit" className='edit-icon' onClick={() => {
+              setOldTodos(todo);
+              setNewTodo(todo);
+              setEditmode(true);
+            }} />
+            <img src={deleteIcon} alt="Delete" className='delete-icon' 
+                onClick = {() => {
+                  deleteTodo(todo);
+                }} />
+          </div>
         </div>
       ))}
       </div>
@@ -42,7 +74,16 @@ function App() {
           onChange={(e) => {
             setNewTodo(e.target.value);
           }}/>
-          <button className='add-button' onClick={addTodo} >Add Todo</button>
+          <button className='add-button'
+            onClick={() => {
+              if(editmode){
+                editTodo();
+              }else{
+                addTodo();
+              }
+            }}>
+              {editmode ? 'Edit Todo' : 'Add Todo'}
+            </button>  
       </div>
 
     </div>
